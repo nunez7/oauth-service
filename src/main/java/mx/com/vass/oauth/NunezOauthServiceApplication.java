81e90637668120c52fcfa89eb6cef2d2c005bf8e
Felix Nunez <felix.nunez@vass.com.mx>
2024-03-11 14:44:07 -0700

    Creacion de proyecto base

---
+++ src/main/java/mx/com/vass/oauth/NunezOauthServiceApplication.java
@@ -0,0 +1,13 @@
+package mx.com.vass.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunezOauthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NunezOauthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/vass/oauth/NunezOauthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.vass.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunezOauthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
